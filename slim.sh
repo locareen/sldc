4cb6abb0c7b67c8d9fc6f74a2484e98f21703026
ksmt4699 <ksmt4699@gmail.com>
2019-09-25 19:03:37 +0900

    バグの修正

--- slim.sh
+++ slim.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -e
+#! /bin/bash
 # checking
 ## check if this runnig as root
 if [ $UID != 0 ]; then
@@ -13,17 +13,19 @@ fi
 WORKDIR=$1
 IMAGENAME=$2
 MOUNTPOINT=$3
-NOUMOUNT=$4
+INITCOMMAND=$4
+NOUMOUNT=$5
 mkdir -p $MOUNTPOINT
 mount -o loop,rw,offset=1048576 $WORKDIR/$IMAGENAME $MOUNTPOINT
 xhost +local: > /dev/null
-exec systemd-nspawn --setenv=DISPLAY=:0 \
+systemd-nspawn --setenv=DISPLAY=:0 \
 --setenv=XAUTHORITY=~/.Xauthority \
 --bind=/tmp/.X11-unix \
+--bind-ro=$HOME/.Xauthority:/home/serene/.Xauthority \
 --bind-ro=/etc/resolv.conf \
 --hostname=SereneLinux-Basix \
 --user=serene \
--D /mnt /home/serene/xinit.sh
-if [[ ! $NOUMOUNT == true ]]; then
-    unmount /mnt || echo "Cannot unmount /mnt" 1>&2
+-D /mnt "$INITCOMMAND"
+if [ $NOUMOUNT == "false" ]; then
+    umount /mnt || echo "Cannot unmount /mnt" 1>&2
 fi
